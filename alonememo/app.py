ddbdf054fa80846f4aeb7e4f495c0d640d0fe69d
Changu Kim <cgkim449@gmail.com>
2022-01-24 16:00:17 +0900

    Update files

--- alonememo/app.py
+++ alonememo/app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 import requests
 from bs4 import BeautifulSoup
-from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만들거나 사용합니다.
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 @app.route('/')
 def home():
